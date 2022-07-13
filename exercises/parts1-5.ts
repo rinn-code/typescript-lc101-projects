ec002743ac404d4f3eac41b0264017544cf28a61
Rinn Mattox <rinnreed@gmail.com>
2022-07-12 20:09:47 -0500

    moved console.log

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -3,11 +3,11 @@
 
 
 // Part 1: Declare (5) Variables With Type
-let spacecraftName:	string = 'Determination';
-let speedMph:	number = 17500;
-let kilometersToMars:	number = 225000000;
-let kilometersToTheMoon:	number = 384400;
-let milesPerKilometer:	number = 0.621;
+let spacecraftName: string = 'Determination';
+let speedMph: number = 17500;
+let kilometersToMars: number = 225000000;
+let kilometersToTheMoon: number = 384400;
+let milesPerKilometer: number = 0.621;
 
 
 // Part 2: Print Days to Mars
@@ -15,10 +15,10 @@ let milesToMars: number = (kilometersToMars * milesPerKilometer);
 let hoursToMars: number = (milesToMars / speedMph);
 let daysToMars: number = (hoursToMars / 24);
 
-console.log(`It will take ${spacecraftName} ${daysToMars} days to reach Mars.`);
+
 
 // Code an output statement here (use a template literal):
-
+console.log(`It will take ${spacecraftName} ${daysToMars} days to reach Mars.`);
 
 
 // Part 3: Create a Function ("getDaysToLocation")
